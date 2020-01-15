81fb6df41bd7f76c62981763cb31c67688585139
ThaiDzai <thaidzai@dzaiBook.local>
2020-01-15 11:48:44 +0700

    Feature Diff done

--- src/sqitch-diff/templates/diff.tpl.go
+++ src/sqitch-diff/templates/diff.tpl.go
@@ -3,13 +3,61 @@ package tpldiff
 // Diff ...
 const Diff = `
 +-- Alter Tables
-{{- range .AlterTables}}+-- {{.Name}}
+{{- if .AlterTables}}
+{{- range .AlterTables}}
++-- {{.Name}}
+|   +-- fields:
 {{- range $fieldIndex, $field := .Fields }}{{$arrChanges := DetectChanges $field}}
-|  +-- {{$field.Name}}
+|   |  +-- {{$field.Name}}
 {{- range $changeIdx, $change := $arrChanges}}
-|  |   +-- {{$change}}
+|   |  |   +-- {{$change}}{{- end}}
 {{- end}}
 {{- end}}
+{{else}}
+Nothing changes
+{{- end}}
+
++-- New Tables
+{{- if .Tables}}
+{{- range .Tables}}
++-- {{.TableName}}
+|   +-- fields:
+{{- range $fieldIndex, $field := .Fields}}{{$arrChanges := DetectChanges $field}}
+{{- if not $field.SkipInDB}}
+|   |   +-- {{$field.Name}}
+|   |   |   +-- type: {{$field.Type}}
+|   |   |   +-- primary: {{$field.Primary}}
+|   |   |   +-- not_null: {{$field.NotNull}}
+|   |   |   +-- unique: {{$field.Unique}}
+|   |   |   +-- Default: {{$field.Default}}
+{{- end}}
+{{- end}}
+|  +-- indexs:
+{{- range $i, $index := .Indexs}}
+|   |   +-- {{$index.Name}}
+|   |   |   +-- key: {{$index.Key}}
+|   |   |   +-- using: {{$index.Using}}
+{{- end}}
+|   +-- histories:
+{{- range $i, $history := .Histories}}
+{{- if $history.IsNoneFields}}
+|   |   +-- None fields
+{{else}}
+|   |   +-- {{$history.Name}}
+{{- end}}
+{{- end}}
+{{- end}}
+{{else}}
+Nothing changes
+{{- end}}
+
++-- Drop Tables
+{{- if .DropTables.Tables}}
+{{- range $index, $table := .DropTables.Tables}}
+|   +-- {{$table}}
+{{- end}}
+{{else}}
+Nothing changes
 {{- end}}
 +--------------------------------------------------------------------+
 `
